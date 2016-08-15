b6d958617c17290b84a0736e56a5219e8d155268
Adam Duren <adam@wearenorthern.com>
2016-08-15 12:27:25 -0400

    Temp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,18 +3,19 @@
 import { addProviders, async, inject } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { provide } from '@angular/core';
+import { TestService } from './test.service';
+
 describe('App: EnvironmentTestBug', () => {
   beforeEach(() => {
-    addProviders([AppComponent]);
+    addProviders([
+      AppComponent,
+      provide(TestService, { useValue: {} }),
+    ]);
   });
 
   it('should create the app',
     inject([AppComponent], (app: AppComponent) => {
       expect(app).toBeTruthy();
-    }));
-
-  it('should have as title \'app works!\'',
-    inject([AppComponent], (app: AppComponent) => {
-      expect(app.title).toEqual('app works!');
     }));
 });
